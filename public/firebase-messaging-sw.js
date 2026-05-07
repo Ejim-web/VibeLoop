0724ee8d86661fff21239a52f834159ffb1922a5
Ejim-web <ejimchinedu24@gmail.com>
2026-05-07 21:57:13 +0100

    Initialize Firebase messaging service in service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,1 +1,14 @@
-
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.0/firebase-messaging-compat.js');
+firebase.initializeApp({
+  apiKey: "AIzaSyCr1IkJ6_SMhTJvY3K3GG6SoIRe1LOZ0nQ",
+  authDomain: "vibeloop-44c45.firebaseapp.com",
+  projectId: "vibeloop-44c45",
+  storageBucket: "vibeloop-44c45.appspot.com",
+  messagingSenderId: "776248914692",
+  appId: "1:776248914692:web:b883192e8498e5a5351651"
+});
+const messaging = firebase.messaging();
+messaging.onBackgroundMessage(payload => {
+  self.registration.showNotification(payload.notification.title, { body: payload.notification.body, icon: '/icon.png' });
+});
