3e1261c68296191ab95d2ed37797777b04af2832
Ejim-web <ejimchinedu24@gmail.com>
2026-07-22 14:31:06 +0100

    Implement service worker with caching strategy

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,39 @@
+const CACHE_NAME = 'vibeloop-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/js/auth.js',
+  '/js/feed.js',
+  '/js/upload.js',
+  '/js/profile.js',
+  '/js/interactions.js',
+  '/js/chat.js',
+  '/js/utils.js',
+  '/manifest.json'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(name => {
+          if (name !== CACHE_NAME) return caches.delete(name);
+        })
+      );
+    })
+  );
+});
